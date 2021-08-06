f57c37e9720f11ba22208291c4c773f655151fdb
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-06 19:03:24 -0400

    Criado a classe TabuleiroConsole.java

---
+++ src/main/java/br/com/bruno/campo_minado/visao/TabuleiroConsole.java
@@ -0,0 +1,16 @@
+package br.com.bruno.campo_minado.visao;
+
+import br.com.bruno.campo_minado.modelo.Tabuleiro;
+
+public class TabuleiroConsole {
+
+    //atributo
+    private Tabuleiro tabuleiro;
+
+    //construtor
+    public TabuleiroConsole(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+
+
+}
