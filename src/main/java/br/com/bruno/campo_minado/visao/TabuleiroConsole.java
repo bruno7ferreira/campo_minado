ffe89e62fe9fe61c5785b1a9a2307d58ddbf86c5
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-06 19:22:12 -0400

    Configurando a classe TabuleiroConsole.java

--- src/main/java/br/com/bruno/campo_minado/visao/TabuleiroConsole.java
+++ src/main/java/br/com/bruno/campo_minado/visao/TabuleiroConsole.java
@@ -1,15 +1,52 @@
 package br.com.bruno.campo_minado.visao;
 
+import br.com.bruno.campo_minado.excecao.ExplosaoException;
+import br.com.bruno.campo_minado.excecao.SairException;
 import br.com.bruno.campo_minado.modelo.Tabuleiro;
+
+import java.util.Scanner;
 
 public class TabuleiroConsole {
 
     //atributo
     private Tabuleiro tabuleiro;
+    private Scanner entrada = new Scanner(System.in);
 
     //construtor
     public TabuleiroConsole(Tabuleiro tabuleiro) {
         this.tabuleiro = tabuleiro;
+
+        executarJogo();
+    }
+
+    private void executarJogo() {
+        try {
+            boolean continuar = true;
+
+            while (continuar) {
+                cicloDoJogo();
+                System.out.println("Quer outra partida? (S/n)");
+                String respota = entrada.nextLine();
+                if ("n".equalsIgnoreCase(respota)) {
+                    continuar = false;
+                } else {
+                    tabuleiro.reiniciar();
+                }
+            }
+        } catch (SairException e) {
+            System.out.println("Thau!");
+        } finally {
+            entrada.close();
+        }
+    }
+
+    private void cicloDoJogo() {
+        try {
+            System.out.println("Você ganhou!");
+        } catch (ExplosaoException e) {
+            System.out.println("Você perdeu! ");
+        }
+
     }
 
 
